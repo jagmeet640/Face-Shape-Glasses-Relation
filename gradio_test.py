8bdf8a5513e0397941d62a4b50a47bda4b82a20d
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-01-27 18:41:29 +0400

    Update gradio_test.py

--- gradio_test.py
+++ gradio_test.py
@@ -1,12 +1,18 @@
+import numpy as np
 import gradio as gr
 
-def greet(name, intensity):
-    return "Hello " * intensity + name + "!"
+# def sepia(input_img):
+#     sepia_filter = np.array([
+#         [0.393, 0.769, 0.189], 
+#         [0.349, 0.686, 0.168], 
+#         [0.272, 0.534, 0.131]
+#     ])
+#     sepia_img = input_img.dot(sepia_filter.T)
+#     sepia_img /= sepia_img.max()
+#     return sepia_img
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
-)
+def prediction_shape(input_image):
+    return input_image
 
+demo = gr.Interface(prediction_shape, gr.Image(), "image")
 demo.launch()
